ba07f4607a7f73c828ebe96d36f9f1280bfa61b5
Alejandro Cantillo <jaimecantillo95@gmail.com>
2020-10-12 22:55:06 -0500

    ADD Se implementa Register en jsx y scss MOD Se modifica Login y App

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -5,25 +5,25 @@ import twitterIcon from '../assets/static/twitter-icon.png';
 
 const Login = () => (
   <section className="login">
-      <section className="login__container">
-        <h2>Inicia sesión</h2>
-        <form className="login__container--form">
-          <input className="input" type="text" placeholder="Correo" />
-          <input className="input" type="password" placeholder="Contraseña" />
-          <button className="button">Iniciar sesión</button>
-          <div className="login__container--remember-me">
-            <label>
-              <input type="checkbox" name="" id="cbox1" value="checkbos" />Recuérdame
-            </label>
-            <a href="/">Olvidé mi contraseña</a>
-          </div>
-        </form>
-        <section className="login__container--social-media">
-          <div><img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
-          <div><img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
-        </section>
-        <p className="login__container--register">No tienes ninguna cuenta <a href="">Regístrate</a></p>
+    <section className="login__container">
+      <h2>Inicia sesión</h2>
+      <form className="login__container--form">
+        <input className="input" type="text" placeholder="Correo" />
+        <input className="input" type="password" placeholder="Contraseña" />
+        <button className="button">Iniciar sesión</button>
+        <div className="login__container--remember-me">
+          <label>
+            <input type="checkbox" id="cbox1" value="first_checkbox" />Recuérdame
+          </label>
+          <a href="/">Olvidé mi contraseña</a>
+        </div>
+      </form>
+      <section className="login__container--social-media">
+        <div><img src={googleIcon} /> Inicia sesión con Google</div>
+        <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
       </section>
+      <p className="login__container--register">No tienes ninguna cuenta <a href="">Regístrate</a></p>
+    </section>
   </section>
 );
 
